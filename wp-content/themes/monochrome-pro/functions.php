6c13b74dc34d12c82aa6898474a2d1c25f53b317
Zarin Neugebauer <zarin@steckinsights.com>
2023-02-13 13:27:13 -0700

    Scottsdale name update

--- wp-content/themes/monochrome-pro/functions.php
+++ wp-content/themes/monochrome-pro/functions.php
@@ -1,13 +1,13 @@
 <?php
 /**
- * Oasis Pro.
+ * Scottsdale Treatment
  *
- * This file adds functions to the Oasis Theme.
+ * 
  *
- * @package Oasis
+ * @package 
  * @author  StudioPress
  * @license GPL-2.0-or-later
- * @link    https://my.studiopress.com/themes/oasis/
+ * @link    https://my.studiopress.com/themes/
  */
 
 // Starts the engine.
@@ -16,15 +16,15 @@ require_once get_template_directory() . '/lib/init.php';
 // Setup Theme.
 require_once get_stylesheet_directory() . '/lib/theme-defaults.php';
 
-add_action( 'after_setup_theme', 'oasis_localization_setup' );
+add_action( 'after_setup_theme', 'monochrome_localization_setup' );
 /**
  * Sets localization (do not remove).
  *
  * @since 1.0.0
  */
-function oasis_localization_setup() {
+function monochrome_localization_setup() {
 
-	load_child_theme_textdomain( 'oasis-pro', get_stylesheet_directory() . '/languages' );
+	load_child_theme_textdomain( 'monochrome-pro', get_stylesheet_directory() . '/languages' );
 
 }
 
@@ -46,7 +46,7 @@ require_once get_stylesheet_directory() . '/lib/woocommerce/woocommerce-output.p
 // Includes notice to install Genesis Connect for WooCommerce.
 require_once get_stylesheet_directory() . '/lib/woocommerce/woocommerce-notice.php';
 
-add_action( 'after_setup_theme', 'oasis_theme_support', 1 );
+add_action( 'after_setup_theme', 'monochrome_theme_support', 1 );
 /**
  * Add desired theme supports.
  *
@@ -54,7 +54,7 @@ add_action( 'after_setup_theme', 'oasis_theme_support', 1 );
  *
  * @since 1.3.0
  */
-function oasis_theme_support() {
+function monochrome_theme_support() {
 
 	$theme_supports = genesis_get_config( 'theme-supports' );
 
@@ -76,24 +76,24 @@ function genesis_child_gutenberg_support() { // phpcs:ignore WordPress.NamingCon
 	require_once get_stylesheet_directory() . '/lib/gutenberg/init.php';
 }
 
-add_action( 'wp_enqueue_scripts', 'oasis_enqueue_scripts_styles' );
+add_action( 'wp_enqueue_scripts', 'monochrome_enqueue_scripts_styles' );
 /**
  * Enqueues scripts and styles.
  *
  * @since 1.0.0
  */
-function oasis_enqueue_scripts_styles() {
+function monochrome_enqueue_scripts_styles() {
 
-	wp_enqueue_style( 'oasis-fonts', '//fonts.googleapis.com/css?family=Muli:300,300i,400,400i,600,600i|Open+Sans+Condensed:300', [], genesis_get_theme_version() );
-	wp_enqueue_style( 'oasis-ionicons', '//unpkg.com/ionicons@4.1.2/dist/css/ionicons.min.css', [], genesis_get_theme_version() );
+	wp_enqueue_style( 'monochrome-fonts', '//fonts.googleapis.com/css?family=Muli:300,300i,400,400i,600,600i|Open+Sans+Condensed:300', [], genesis_get_theme_version() );
+	wp_enqueue_style( 'monochrome-ionicons', '//unpkg.com/ionicons@4.1.2/dist/css/ionicons.min.css', [], genesis_get_theme_version() );
 
-	wp_enqueue_script( 'oasis-global-script', get_stylesheet_directory_uri() . '/js/global.js', [ 'jquery' ], '1.0.0', true );
-	wp_enqueue_script( 'oasis-block-effects', get_stylesheet_directory_uri() . '/js/block-effects.js', [], '1.0.0', true );
+	wp_enqueue_script( 'monochrome-global-script', get_stylesheet_directory_uri() . '/js/global.js', [ 'jquery' ], '1.0.0', true );
+	wp_enqueue_script( 'monochrome-block-effects', get_stylesheet_directory_uri() . '/js/block-effects.js', [], '1.0.0', true );
 
 	$suffix = ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ? '' : '.min';
 
-	wp_enqueue_script( 'oasis-responsive-menu', get_stylesheet_directory_uri() . '/js/responsive-menus' . $suffix . '.js', [ 'jquery' ], genesis_get_theme_version(), true );
-	wp_localize_script( 'oasis-responsive-menu', 'genesis_responsive_menu', oasis_responsive_menu_settings() );
+	wp_enqueue_script( 'monochrome-responsive-menu', get_stylesheet_directory_uri() . '/js/responsive-menus' . $suffix . '.js', [ 'jquery' ], genesis_get_theme_version(), true );
+	wp_localize_script( 'monochrome-responsive-menu', 'genesis_responsive_menu', monochrome_responsive_menu_settings() );
 
 }
 
@@ -102,12 +102,12 @@ function oasis_enqueue_scripts_styles() {
  *
  * @since 1.1.0
  */
-function oasis_responsive_menu_settings() {
+function monochrome_responsive_menu_settings() {
 
 	$settings = [
-		'mainMenu'         => __( 'Menu', 'oasis-pro' ),
+		'mainMenu'         => __( 'Menu', 'monochrome-pro' ),
 		'menuIconClass'    => 'ionicons-before ion-ios-menu',
-		'subMenu'          => __( 'Submenu', 'oasis-pro' ),
+		'subMenu'          => __( 'Submenu', 'monochrome-pro' ),
 		'subMenuIconClass' => 'ionicons-before ion-ios-arrow-down',
 		'menuClasses'      => [
 			'combine' => [],
@@ -125,7 +125,7 @@ function oasis_responsive_menu_settings() {
 add_image_size( 'featured-blog', 600, 338, true );
 add_image_size( 'sidebar-thumbnail', 80, 80, true );
 
-add_filter( 'image_size_names_choose', 'oasis_media_library_sizes' );
+add_filter( 'image_size_names_choose', 'monochrome_media_library_sizes' );
 /**
  * Adds featured-blog image size to Media Library.
  *
@@ -134,9 +134,9 @@ add_filter( 'image_size_names_choose', 'oasis_media_library_sizes' );
  * @param array $sizes Array of image sizes and their names.
  * @return array The modified list of sizes.
  */
-function oasis_media_library_sizes( $sizes ) {
+function monochrome_media_library_sizes( $sizes ) {
 
-	$sizes['featured-blog'] = __( 'Featured Blog - 600px by 338px', 'oasis-pro' );
+	$sizes['featured-blog'] = __( 'Featured Blog - 600px by 338px', 'monochrome-pro' );
 
 	return $sizes;
 
@@ -161,24 +161,24 @@ add_action( 'genesis_header', 'genesis_do_nav', 12 );
 remove_action( 'genesis_after_header', 'genesis_do_subnav' );
 add_action( 'genesis_after', 'genesis_do_subnav', 12 );
 
-add_action( 'genesis_meta', 'oasis_add_search_icon' );
+add_action( 'genesis_meta', 'monochrome_add_search_icon' );
 /**
  * Adds the search icon to the header if the option is set in the Customizer.
  *
  * @since 1.0.0
  */
-function oasis_add_search_icon() {
+function monochrome_add_search_icon() {
 
-	$show_icon = get_theme_mod( 'oasis_header_search', oasis_customizer_get_default_search_setting() );
+	$show_icon = get_theme_mod( 'monochrome_header_search', monochrome_customizer_get_default_search_setting() );
 
 	// Exit early if option set to false.
 	if ( ! $show_icon ) {
 		return;
 	}
 
-	add_action( 'genesis_header', 'oasis_do_header_search_form', 14 );
-	add_filter( 'genesis_nav_items', 'oasis_add_search_menu_item', 10, 2 );
-	add_filter( 'wp_nav_menu_items', 'oasis_add_search_menu_item', 10, 2 );
+	add_action( 'genesis_header', 'monochrome_do_header_search_form', 14 );
+	add_filter( 'genesis_nav_items', 'monochrome_add_search_menu_item', 10, 2 );
+	add_filter( 'wp_nav_menu_items', 'monochrome_add_search_menu_item', 10, 2 );
 
 }
 
@@ -191,9 +191,9 @@ function oasis_add_search_icon() {
  * @param array  $args The menu options.
  * @return string Updated menu HTML.
  */
-function oasis_add_search_menu_item( $items, $args ) {
+function monochrome_add_search_menu_item( $items, $args ) {
 
-	$search_toggle = sprintf( '<li class="menu-item">%s</li>', oasis_get_header_search_toggle() );
+	$search_toggle = sprintf( '<li class="menu-item">%s</li>', monochrome_get_header_search_toggle() );
 
 	if ( 'primary' === $args->theme_location ) {
 		$items .= $search_toggle;
@@ -203,7 +203,7 @@ function oasis_add_search_menu_item( $items, $args ) {
 
 }
 
-add_filter( 'wp_nav_menu_args', 'oasis_secondary_menu_args' );
+add_filter( 'wp_nav_menu_args', 'monochrome_secondary_menu_args' );
 /**
  * Reduces secondary navigation menu to one level depth.
  *
@@ -212,7 +212,7 @@ add_filter( 'wp_nav_menu_args', 'oasis_secondary_menu_args' );
  * @param array $args Original menu options.
  * @return array Menu options with depth set to 1.
  */
-function oasis_secondary_menu_args( $args ) {
+function monochrome_secondary_menu_args( $args ) {
 
 	if ( 'secondary' === $args['theme_location'] ) {
 		$args['depth'] = 1;
@@ -222,7 +222,7 @@ function oasis_secondary_menu_args( $args ) {
 
 }
 
-add_filter( 'genesis_author_box_gravatar_size', 'oasis_author_box_gravatar' );
+add_filter( 'genesis_author_box_gravatar_size', 'monochrome_author_box_gravatar' );
 /**
  * Modifies size of the Gravatar in the author box.
  *
@@ -231,13 +231,13 @@ add_filter( 'genesis_author_box_gravatar_size', 'oasis_author_box_gravatar' );
  * @param int $size Original icon size.
  * @return int Modified icon size.
  */
-function oasis_author_box_gravatar( $size ) {
+function monochrome_author_box_gravatar( $size ) {
 
 	return 90;
 
 }
 
-add_filter( 'genesis_comment_list_args', 'oasis_comments_gravatar' );
+add_filter( 'genesis_comment_list_args', 'monochrome_comments_gravatar' );
 /**
  * Modifies size of the Gravatar in the entry comments.
  *
@@ -246,14 +246,14 @@ add_filter( 'genesis_comment_list_args', 'oasis_comments_gravatar' );
  * @param array $args Gravatar settings.
  * @return array Gravatar settings with modified size.
  */
-function oasis_comments_gravatar( $args ) {
+function monochrome_comments_gravatar( $args ) {
 
 	$args['avatar_size'] = 48;
 	return $args;
 
 }
 
-add_filter( 'get_the_content_limit', 'oasis_content_limit_read_more_markup', 10, 3 );
+add_filter( 'get_the_content_limit', 'monochrome_content_limit_read_more_markup', 10, 3 );
 /**
  * Modifies the generic more link markup for posts.
  *
@@ -264,7 +264,7 @@ add_filter( 'get_the_content_limit', 'oasis_content_limit_read_more_markup', 10,
  * @param string $link The link HTML.
  * @return string The new more link HTML.
  */
-function oasis_content_limit_read_more_markup( $output, $content, $link ) {
+function monochrome_content_limit_read_more_markup( $output, $content, $link ) {
 
 	$output = sprintf( '<p>%s &#x02026;</p><p class="more-link-wrap">%s</p>', $content, str_replace( '&#x02026;', '', $link ) );
 
@@ -277,13 +277,13 @@ remove_action( 'genesis_entry_footer', 'genesis_entry_footer_markup_open', 5 );
 remove_action( 'genesis_entry_footer', 'genesis_post_meta' );
 remove_action( 'genesis_entry_footer', 'genesis_entry_footer_markup_close', 15 );
 
-add_action( 'genesis_before_footer', 'oasis_before_footer_cta' );
+add_action( 'genesis_before_footer', 'monochrome_before_footer_cta' );
 /**
  * Hooks in before footer CTA widget area.
  *
  * @since 1.0.0
  */
-function oasis_before_footer_cta() {
+function monochrome_before_footer_cta() {
 
 	genesis_widget_area(
 		'before-footer-cta',
@@ -305,15 +305,15 @@ add_action( 'genesis_after', 'genesis_footer_markup_open', 5 );
 add_action( 'genesis_after', 'genesis_do_footer' );
 add_action( 'genesis_after', 'genesis_footer_markup_close', 15 );
 
-add_action( 'genesis_after', 'oasis_custom_footer_logo', 7 );
+add_action( 'genesis_after', 'monochrome_custom_footer_logo', 7 );
 /**
  * Outputs the footer logo above the footer credits.
  *
  * @since 1.2.0
  */
-function oasis_custom_footer_logo() {
+function monochrome_custom_footer_logo() {
 
-	$footer_logo      = get_theme_mod( 'oasis-footer-logo', oasis_get_default_footer_logo() );
+	$footer_logo      = get_theme_mod( 'monochrome-footer-logo', monochrome_get_default_footer_logo() );
 	$footer_logo_link = sprintf( '<p><a class="footer-logo-link" href="%1$s"><img class="footer-logo" src="%2$s" alt="%3$s" /></a></p>', trailingslashit( home_url() ), esc_url( $footer_logo ), get_bloginfo( 'name' ) );
 
 	if ( $footer_logo ) {
@@ -326,7 +326,7 @@ function oasis_custom_footer_logo() {
 genesis_register_sidebar(
 	[
 		'id'          => 'before-footer-cta',
-		'name'        => __( 'Before Footer CTA', 'oasis-pro' ),
-		'description' => __( 'This is the before footer CTA section.', 'oasis-pro' ),
+		'name'        => __( 'Before Footer CTA', 'monochrome-pro' ),
+		'description' => __( 'This is the before footer CTA section.', 'monochrome-pro' ),
 	]
 );
