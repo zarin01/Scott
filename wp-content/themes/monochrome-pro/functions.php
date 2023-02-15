2ed2d4cee1e8bd5c207ffa62758bd719ee38869b
zarin01 <zarinneugebauer@gmail.com>
2023-02-15 13:33:13 -0700

    favicons and css changes

--- wp-content/themes/monochrome-pro/functions.php
+++ wp-content/themes/monochrome-pro/functions.php
@@ -370,3 +370,20 @@ genesis_register_sidebar(
 		'description' => __( 'This is the above navbar CTA.', 'monochrome-pro' ),
 	]
 );
+
+
+
+
+function my_login_logo() { ?>
+	<style type=”text/css”>
+	#login h1 a, .login h1 a {
+	background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/images/login-logo.png);
+	height:65px;
+	width:320px;
+	background-size: 320px 65px;
+	background-repeat: no-repeat;
+	padding-bottom: 30px;
+	}
+	</style>
+	<?php }
+	add_action( 'login_enqueue_scripts', 'my_login_logo' );
